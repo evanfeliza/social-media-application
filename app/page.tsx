197355521044ba7cc43e25b5a656a8d169ad473e
dumvan <felizavndrd567@gmail.com>
2024-04-09 02:10:28 +0800

    feat: app landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,21 @@
-import DeployButton from "../components/DeployButton";
-import AuthButton from "../components/AuthButton";
-import { createClient } from "@/utils/supabase/server";
-import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
-import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
-import Header from "@/components/Header";
+import DarkModeToggle from "@/components/libs/dark-mode-toggle";
+import Link from "next/link";
 
 export default async function Index() {
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient();
-      return true;
-    } catch (e) {
-      return false;
-    }
-  };
 
-  const isSupabaseConnected = canInitSupabaseClient();
-
-  return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          <DeployButton />
-          {isSupabaseConnected && <AuthButton />}
+    return (
+        <div className="relative hero min-h-screen bg-base-200">
+            <div className="absolute top-3 left-3">
+                <DarkModeToggle />
+            </div>
+            <div className="hero-content text-center">
+                <div className="max-w-lg">
+                    <h1 className="text-7xl font-bold tracking-wider"><span className="text-primary">Connect</span>opia</h1>
+                    <p className="py-6 text-lg">Where Connections Flourish. Join now for authentic <span className="text-secondary">interactions</span> and <span className="text-secondary">vibrant</span> communities</p>
+                    <Link href="/auth/callback">
+                        <button className="btn btn-primary uppercase" >Join Now <i className="fi fi-rs-earth-americas"></i></button></Link>
+                </div>
+            </div>
         </div>
-      </nav>
-
-      <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
-        <Header />
-        <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-        </main>
-      </div>
-
-      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-        <p>
-          Powered by{" "}
-          <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-            target="_blank"
-            className="font-bold hover:underline"
-            rel="noreferrer"
-          >
-            Supabase
-          </a>
-        </p>
-      </footer>
-    </div>
-  );
+    );
 }
