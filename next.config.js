58892d5ec2c0cc2b25dcc6ade21e7817bbc3ffc7
dumvan <felizavndrd567@gmail.com>
2024-04-09 02:06:28 +0800

    chore: add dependencies

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = { 
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.dicebear.com',
+        port: '',
+        pathname: '/8.x/initials/**',
+      },
+    ],
+  },
+
+};
 
 module.exports = nextConfig;
