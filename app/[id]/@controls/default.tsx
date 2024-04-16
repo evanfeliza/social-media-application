1c391536695b483f7dd24d7c3a009d0cc1e382ff
dumvan <felizavndrd567@gmail.com>
2024-04-17 06:12:35 +0800

    chore: fix profile handle not refreshing

--- app/[id]/@controls/default.tsx
+++ app/[id]/@controls/default.tsx
@@ -1,36 +1,23 @@
-import Link from 'next/link'
-import React from 'react'
-import ProfileHandle from '@/components/libs/profile-handle'
-import { createClient } from '@/utils/supabase/server'
+"use client"
+import React, { useEffect } from 'react'
 
-const ControlsSection = async () => {
-    const supabase = createClient()
+import SidebarControls from '@/components/libs/sidebar'
+import QueryProvider, { useQueryClient } from '@/components/providers/query-provider'
 
-    const { data: { user } } = await supabase.auth.getUser()
+
+
+const ControlsSection = () => {
+    const queryClient = useQueryClient()
+
+    useEffect(() => {
+        queryClient.refetchQueries({
+            queryKey: ['profileInfo']
+        })
+    }, [queryClient])
     return (
-        <div className="h-full drawer drawer-open menu bg-base-300 rounded-box " >
-            <ul className="menu-md h-full w-full">
-                <li>
-                    <Link href={`/${user?.id}/settings`}>
-                        <ProfileHandle />
-                    </Link>
-                </li>
-
-                <li>
-                    <Link href={`//${user?.id}/liked-posts`}>
-                        <i className="fi fi-rr-heart mt-1"></i>
-                        <span >Liked Posts</span>
-                    </Link>
-                </li>
-
-                <li className='w-full'>
-                    <Link href="/auth/logout">
-                        <i className="fi fi-rr-power mt-1"></i>
-                        <span className='not-italic '>Signout</span>
-                    </Link>
-                </li>
-            </ul>
-        </div >
+        <QueryProvider>
+            <SidebarControls />
+        </QueryProvider>
     )
 }
 
