e118c5771186f9647af8d901e1f951ce6ad99f0d
dumvan <felizavndrd567@gmail.com>
2024-04-21 19:55:53 +0800

    chore: add relationship parallel page

---
+++ app/[id]/[settings]/@relationships/default.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const RelationshipsInformationSection = (props: Props) => {
+    return (
+        <div>default</div>
+    )
+}
+
+export default RelationshipsInformationSection
